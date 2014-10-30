9a38941ff16ba490b4da8e04652807ba9de63634
Yanglei Li <yanglei.li@searshc.com>
2014-10-30 01:19:28 -0700

    Added sitemap.xml. Added ads to YTP right side column

---
+++ beta/ytp/local/rside.php
@@ -0,0 +1,11 @@
+<table class="note" bgcolor="#F9E3E3">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- blog -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-4414960646629939"
+     data-ad-slot="4242734405"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</table>
--- beta/ytp/theme/cloud/footer.php
+++ beta/ytp/theme/cloud/footer.php
@@ -1,4 +1,6 @@
-</td>
+</td><?php if ($pageHideRight!=1) { ?>
+  <td valign="top" width="1"><?php include('local/rside.php'); ?></td>
+  <?php } ?>  
   </tr>
 </table>
 <?php include('local/copyr.php'); ?>
